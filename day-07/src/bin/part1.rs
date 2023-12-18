953a4745bf72136059f55ad664379c5ba4a2099f
Joakim Bergros <jbv87@protonmail.com>
2023-12-18 14:35:55 +0100

    Some ordering

--- day-07/src/bin/part1.rs
+++ day-07/src/bin/part1.rs
@@ -1,12 +1,4 @@
-use std::ops::Range;
-
-use nom::{
-    bytes::complete::tag,
-    character::complete::{multispace0, newline, u32},
-    multi::separated_list1,
-    sequence::{preceded, tuple},
-    IResult,
-};
+use std::cmp::Ordering;
 
 fn main() {
     let str = include_str!("part1.txt");
@@ -15,49 +7,81 @@ fn main() {
 }
 
 fn process_input(input: &str) -> String {
-    let (_, values) = parse(input).expect("parse should have worked");
-
-    let possibilities = values
-        .into_iter()
-        .map(|run| {
-            let range = Range {
-                start: 0,
-                end: run.0,
-            };
-            range
-                .into_iter()
-                .filter_map(|travel_speed| {
-                    let remaining_time = run.0 - travel_speed;
-                    let length = travel_speed * remaining_time;
-                    match length > run.1 {
-                        true => Some(length),
-                        false => None,
-                    }
-                })
-                .count() as u32
-        })
-        .product::<u32>();
-
-    possibilities.to_string()
+    dbg!(parse(input));
+    "".to_string()
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<(u32, u32)>> {
-    let (input, holds) = preceded(
-        tuple((tag("Time:"), multispace0)),
-        separated_list1(multispace0, u32),
-    )(input)?;
-    let (input, times) = preceded(
-        tuple((newline, tag("Distance:"), multispace0)),
-        separated_list1(multispace0, u32),
-    )(input)?;
+fn parse(input: &str) -> Vec<Hand> {
+    input
+        .lines()
+        .map(|line| {
+            let mut line_split = line.split(' ');
+            Hand {
+                cards: line_split
+                    .next()
+                    .expect("should be the cards")
+                    .chars()
+                    .map(|c| Card(c))
+                    .collect(),
+                bid: line_split
+                    .last()
+                    .expect("shomsguld be the bid")
+                    .parse::<u32>()
+                    .expect("should be a number"),
+            }
+        })
+        .collect()
+}
 
-    let mut runs: Vec<(u32, u32)> = vec![];
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<Card>,
+    bid: u32,
+}
 
-    for index in 0..=(holds.len() - 1) {
-        runs.push((holds[index], times[index]));
+impl Hand {
+    fn strength(&self) -> u32 {
+        0
     }
 
-    Ok((input, runs))
+    fn get_value(&self) -> u32 {
+        self.cards.iter().map(|card| card.get_value()).sum()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Card(char);
+
+impl Card {
+    fn get_value(&self) -> u32 {
+        match self.0 {
+            'A' => 13,
+            'K' => 12,
+            'Q' => 11,
+            'J' => 10,
+            'T' => 9,
+            '9' => 8,
+            '8' => 7,
+            '7' => 6,
+            '6' => 5,
+            '5' => 4,
+            '4' => 3,
+            '3' => 2,
+            '2' => 1,
+            _ => unreachable!("no card with other values should eixst"),
+        }
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let result = self.get_value() as i32 - other.get_value() as i32;
+        match result {
+            0 => Some(Ordering::Equal),
+            val if val > 0 => Some(Ordering::Greater),
+            _ => Some(Ordering::Less),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -65,9 +89,20 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_card_ordering() {
+        let card1 = Card('A');
+        let card2 = Card('T');
+
+        assert!(card1 > card2);
+    }
+
+    #[test]
     fn test_input() {
-        let str = "Time:      7  15   30
-Distance:  9  40  200
+        let str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
 ";
 
         assert_eq!("288", process_input(str));
