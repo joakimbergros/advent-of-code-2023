d04d195b9c8bb4d71ca344a3dd1cc02c4a60395a
Joakim Bergros <jbv87@protonmail.com>
2023-12-22 12:54:29 +0100

    day 8 part 1 complete

--- day-08/src/bin/part1.rs
+++ day-08/src/bin/part1.rs
@@ -1,11 +1,59 @@
+use std::collections::BTreeMap;
+
 fn main() {
-    let str = include_str!("part2.txt");
+    let str = include_str!("part1.txt");
     let sum = process_input(str);
     println!("{sum}");
 }
 
 fn process_input(input: &str) -> String {
-    String::from("")
+    let choices = input.lines().next().unwrap().chars().collect::<Vec<char>>();
+
+    let maps = input
+        .lines()
+        .skip(2)
+        .map(|line| {
+            line.replace(['(', ')'], "")
+                .split_once(" = ")
+                .map(|(id, paths)| {
+                    (
+                        id.to_string(),
+                        paths
+                            .split_once(", ")
+                            .map(|(l, r)| (l.to_string(), r.to_string()))
+                            .expect("should be left and right"),
+                    )
+                })
+                .expect("should be a valid map")
+        })
+        .collect::<BTreeMap<String, (String, String)>>();
+
+    let mut current = maps
+        .iter()
+        .find(|map| *map.0 == String::from("AAA"))
+        .expect("should always be a entry id");
+    let goal_id = String::from("ZZZ");
+    let mut steps: u32 = 0;
+
+    for choice in choices.iter().cycle() {
+        if *current.0 == goal_id {
+            return steps.to_string();
+        }
+
+        let next = match choice {
+            'R' => &current.1 .1,
+            'L' => &current.1 .0,
+            _ => unreachable!("should be no other choice"),
+        };
+
+        current = maps
+            .iter()
+            .find(|map| map.0 == next)
+            .expect("id of next should exist");
+        steps += 1;
+    }
+
+    "fall".to_string()
 }
 
 #[cfg(test)]
